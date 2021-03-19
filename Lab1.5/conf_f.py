3b54fea6ad0f56a66d10d186550efb572b7f9c78
a.khristoforov <a.khristoforov@jet.su>
2021-03-19 16:28:23 +0300

    Lab1.5

--- Lab1.5/conf_f.py
+++ Lab1.5/conf_f.py
@@ -8,8 +8,10 @@ for name in glob.glob('C:/Utils/config_files/*.txt'):
     with open(name) as f:
         for line in f:
             p = line.find("ip address")
-            if p > 0: res.append(line[p+11:p+46])
-list(set(res))
+            if p > 0: res.append(line[p+11:].strip("\n"))
+
 print(res)
+
+
 
 
