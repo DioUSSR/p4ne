fcf5e7701f7622b3ea8c1debe0e553d2c474a190
a.khristoforov <a.khristoforov@jet.su>
2021-03-22 20:23:51 +0300

    1

---
+++ Lab2.1/api.py
@@ -0,0 +1,17 @@
+import requests, json, pprint
+
+host = '10.31.70.210'
+login = 'restapi'
+password = 'j0sg1280-7@'
+
+url = 'https://' + host + ':55443'
+
+r = requests.post(url + '/api/v1/auth/token-services', auth=(login, password), verify=False)
+
+token = r.json()["token-id"]
+
+header = {"content-type": "application/json", "X-Auth-Token": token}
+
+r = requests.get(url + '/api/v1/interfaces', headers=header, verify=False)
+
+pprint.pprint(r.json())
