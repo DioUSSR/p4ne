268d6295c4b43ebf48c9e87433c8fa67b35347c7
a.khristoforov <a.khristoforov@jet.su>
2021-03-23 11:26:50 +0300

    1

--- Lab1.6/RegEx.py
+++ Lab1.6/RegEx.py
@@ -2,7 +2,7 @@ import glob, re
 res = list()
 def Classificator(x):
     int = re.match(r'(interface)(.+)', x)
-    ip = re.search(r'(([0-9]{1,3}[.]){3}([0-9]{1,3}[\ ]){1}((255[.])([0-9]{1,3}[.\ ]){3}))', x)
+    ip = re.search(r'ip address (([0-9]{1,3}[.]){3}([0-9]{1,3}[\ ]([0-9]{1,3}[.]){3}[0-9]{1,3}))', x)
     host = re.match(r'(hostname)(.+)', x)
     if bool(ip) is True: return {'ip':ip.group(1)}
     if bool(int) is True: return {'interface':int.group(2)}
